e06cdc5244342886be06210793b7a3f0f1113f6e
Pusikovsa <s.puzikov59@gmail.com>
2023-09-18 10:29:28 +0300

    added futureBuilding from widget access and access with the ability to save the phone number when restarting

--- lib/widgets/access_widget.dart
+++ lib/widgets/access_widget.dart
@@ -3,27 +3,30 @@ import 'package:opening_the_barrier/models/phone.dart';
 import 'package:opening_the_barrier/pages/added_phone_page.dart';
 import 'package:provider/provider.dart';
 
-class AccessWidget extends StatelessWidget{
-
+class AccessWidget extends StatelessWidget {
   const AccessWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
-
-    return GestureDetector(
-      child: Column(
-        children: [
-          Text(
-            Provider.of<Phone>(context).getNumber() ?? 'Здесь пока пусто',
-          style: const TextStyle(
-            fontSize: 26,
-          ),),
-          const Text('Для изменения нажмите сюда'),
-        ],
-      ),
-      onTap: () {
-        Navigator.pushNamed(context, AddedPhone.rout);
-      },
-    );
+    return FutureBuilder(
+        future: Provider.of<Phone>(context, listen: false).changeNumber(),
+        builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
+          return GestureDetector(
+            child: Column(
+              children: [
+                Text(
+                  Provider.of<Phone>(context).getNumber() ?? 'Здесь пока пусто',
+                  style: const TextStyle(
+                    fontSize: 26,
+                  ),
+                ),
+                const Text('Для изменения нажмите сюда'),
+              ],
+            ),
+            onTap: () {
+              Navigator.pushNamed(context, AddedPhone.rout);
+            },
+          );
+        });
   }
 }
